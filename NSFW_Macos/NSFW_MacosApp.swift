14cc7c8d0c046218348206e98f85853976d29b49
dacaiguoguo <dacaiguoguo@163.com>
2024-04-22 19:57:39 +0800

    Initial Commit

---
+++ NSFW_Macos/NSFW_MacosApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSFW_MacosApp.swift
+//  NSFW_Macos
+//
+//  Created by yanguo sun on 2024/4/22.
+//
+
+import SwiftUI
+
+@main
+struct NSFW_MacosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
